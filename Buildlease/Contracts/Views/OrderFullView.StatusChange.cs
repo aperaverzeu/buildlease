bf812c454ec28554b940de7a54da4eab5b975e37
Rigorich <nikita.grishaev@mail.ru>
2021-10-26 16:47:50 +0300

    views fixes

---
+++ Buildlease/Contracts/Views/OrderFullView.StatusChange.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Contracts.Views
+{
+    public class StatusChangeView
+    {
+        public DateTime Date { get; set; }
+        public OrderStatus NewStatus { get; set; }
+    }
+}
