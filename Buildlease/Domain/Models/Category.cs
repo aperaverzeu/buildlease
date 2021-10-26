f026f5244049953bbefdb58a59e737b5e527f33e
Rastsislau Kayko <krastsislau@gmail.com>
2021-10-26 16:24:15 +0300

    category model small fix

--- Buildlease/Domain/Models/Category.cs
+++ Buildlease/Domain/Models/Category.cs
@@ -5,5 +5,6 @@
         public int Id { get; set; }
         public int ParentId { get; set; }
         public string Name { get; set; }
+        public string DefaultImagePrefPath { get; set; } // path to an image that is to be displayed for the category's products that don't have an image
     }
 }
