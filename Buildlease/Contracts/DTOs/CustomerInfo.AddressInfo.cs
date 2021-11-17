2603cdc241a42272af7f4c1e99396fb9603b4008
Rigorich <nikita.grishaev@mail.ru>
2021-11-17 23:45:44 +0300

    Customer info

---
+++ Buildlease/Contracts/DTOs/CustomerInfo.AddressInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace Contracts.DTOs
+{
+    public class AddressInfo
+    {
+        public int? Id { get; set; }
+        public string MailIndex { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string Building { get; set; }
+        public string Office { get; set; }
+    }
+}
---
+++ Buildlease/Domain/Models/Customer.Address.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string CustomerId { get; set; }
+        public int Priority { get; set; }
+
+        public string MailIndex { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string Building { get; set; }
+        public string Office { get; set; }
+    }
+}
