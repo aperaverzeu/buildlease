69b2978ae3fe4aa14bb0d4d1b2025fb08a02e20a
Никита Григорьевич Гришаев <nikita.grishaev@mail.ru>
2021-11-19 23:04:46 +0300

    Some models and DTOs (#15)
    * historicity
    
    * Customer info
    
    * renaming

---
+++ Buildlease/Domain/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Customer
+    {
+        public string UserId { get; set; }
+        public string CompanyName { get; set; }
+        public string RepresentativeName { get; set; }
+        public string ContactInfo { get; set; }
+        public int? JuridicalAddressId { get; set; }
+    }
+}
---
+++ Buildlease/Domain/Models/Historicity/HistoryOfProduct.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Domain.Models.Historicity
+{
+    public class HistoryOfProduct
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public DateTime Date { get; set; }
+        public string SerializedProductFullView { get; set; }
+    }
+}
