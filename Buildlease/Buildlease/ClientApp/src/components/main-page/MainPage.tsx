b0f9dd46863cf985e7843ecdb2dd71bdd77e2f57
Никита Григорьевич Гришаев <nikita.grishaev@mail.ru>
2021-11-21 22:36:54 +0300

    fixing categories loading (#22)

--- Buildlease/Buildlease/ClientApp/src/components/main-page/MainPage.tsx
+++ Buildlease/Buildlease/ClientApp/src/components/main-page/MainPage.tsx
@@ -1,23 +1,25 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { Container } from 'reactstrap';
 import Globals from '../../Globals';
 
 import BigHeader from './BigHeader';
 
-export class MainPage extends Component {
-    render() {
-        return (
-            <>
-                <BigHeader />
-                <Container>
-                    <h1>Hello, world!</h1>
-                    {Globals.Categories ?
-                        <p>This is to be the content of the main page, i.e. "our categories", "popular", and "featured"</p>
-                    :
-                        <h1>YOU SHOULD NOT SEE THIS, MORTAL ONE!</h1>
-                    }
-                </Container>
-            </>
-        );
-    }
+export default function MainPage() {
+
+    const [OK, setOK] = useState<boolean>(Globals.Categories !== undefined);
+    if (!OK) Globals.OnCategoriesLoadedListeners!.push(() => setOK(true));
+
+    return (
+        <>
+            <BigHeader />
+            <Container>
+                <h1>Hello, world!</h1>
+                {OK ?
+                    <p>This is to be the content of the main page, i.e. "our categories", "popular", and "featured"</p>
+                :
+                    <h1>YOU SHOULD NOT SEE THIS, MORTAL ONE!</h1>
+                }
+            </Container>
+        </>
+    );
 }
