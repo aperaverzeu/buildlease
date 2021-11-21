380a342783433d52214a010bb068d3f378d99163
Rigorich <nikita.grishaev@mail.ru>
2021-11-21 19:27:08 +0300

    uncommit first sprint models

--- Buildlease/Persistence/ApplicationDbContext.cs
+++ Buildlease/Persistence/ApplicationDbContext.cs
@@ -11,13 +11,13 @@ namespace Persistence
     {
         public DbSet<TestModel> TestModels { get; set; }
 
-        //public DbSet<Attribute> Attributes { get; set; }
-        //public DbSet<Category> Categories { get; set; }
+        public DbSet<Attribute> Attributes { get; set; }
+        public DbSet<Category> Categories { get; set; }
         //public DbSet<Address> CustomerAddresses { get; set; }
         //public DbSet<Customer> Customers { get; set; }
         //public DbSet<Order> Orders { get; set; }
-        //public DbSet<Product> Products { get; set; }
-        //public DbSet<ProductAttribute> ProductAttributes { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductAttribute> ProductAttributes { get; set; }
         //public DbSet<ProductOrder> ProductOrders { get; set; }
 
         //public DbSet<HistoryOfOrderStatus> HistoryOfOrderStatus { get; set; }
