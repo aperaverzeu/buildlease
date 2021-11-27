74c44d294115f6175310ac36c4ff15bde6b7d188
gorobot <gorobot.nz@gmail.com>
2021-11-27 16:29:25 +0300

    Seconds

--- Buildlease/Domain/EntitiesExample/OrderStatusHistoryEntities.cs
+++ Buildlease/Domain/EntitiesExample/OrderStatusHistoryEntities.cs
@@ -15,28 +15,28 @@ namespace Domain.EntitiesExample
                 {
                     Id = 1,
                     OrderId = 4,
-                    Date = new DateTime(2020, 9, 20, 18, 22, 0),
+                    Date = new DateTime(2020, 9, 20, 18, 22, 15),
                     NewStatus = OrderStatus.WaitingForApproval
                 },
                 new HistoryOfOrderStatus()
                 {
                     Id = 2,
                     OrderId = 4,
-                    Date = new DateTime(2020, 9, 25, 14, 14, 0),
+                    Date = new DateTime(2020, 9, 25, 14, 14, 16),
                     NewStatus = OrderStatus.Approved
                 },
                 new HistoryOfOrderStatus()
                 {
                     Id = 3,
                     OrderId = 4,
-                    Date = new DateTime(2020, 10, 11, 12, 30, 0),
+                    Date = new DateTime(2020, 10, 11, 12, 30, 17),
                     NewStatus = OrderStatus.DocumentPending
                 },
                 new HistoryOfOrderStatus()
                 {
                     Id = 4,
                     OrderId = 4,
-                    Date = new DateTime(2020, 10, 12, 13, 0, 0),
+                    Date = new DateTime(2020, 10, 12, 13, 0, 18),
                     NewStatus = OrderStatus.InProcess
                 },
                 new HistoryOfOrderStatus()
