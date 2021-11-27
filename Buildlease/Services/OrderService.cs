17b694c67bd3d6776815060ab9a95cd69963187f
Rigorich <nikita.grishaev@mail.ru>
2021-11-27 11:41:38 +0300

    implimenting OrderService

--- Buildlease/Services/OrderService.cs
+++ Buildlease/Services/OrderService.cs
@@ -2,6 +2,7 @@
 using Domain.Models;
 using Persistence;
 using Services.Abstractions;
+using Services.Extension;
 using System.Linq;
 
 namespace Services
@@ -15,19 +16,64 @@ namespace Services
         public CartFullView GetMyCart(string userId)
         {
             var order = _db.Orders.SingleOrDefault(e => e.Status == OrderStatus.Cart);
-            var productOrders = _db.ProductOrders.Where(e => e.OrderId == order.Id);
-            // ...
-            throw new System.NotImplementedException();
+            var productOrderViews = _db.GetProductOrderViews(order.Id);
+            var cartFullView = new CartFullView()
+            {
+                ProductOrders = productOrderViews,
+            };
+            return cartFullView;
         }
 
         public OrderView[] GetMyOrders(string userId)
         {
-            throw new System.NotImplementedException();
+            var orders = _db.Orders.Where(e => e.CustomerId == userId);
+
+            var orderViews = orders
+                .Select(e => new OrderView()
+                {
+                    Id = e.Id,
+                    Status = e.Status,
+                    ProductOrders = _db.GetProductOrderViews(e.Id),
+                })
+                .ToArray();
+
+            foreach (var orderView in orderViews)
+            {
+                orderView.ProductCount = orderView.ProductOrders.Sum(po => po.Count);
+                orderView.Price = orderView.ProductOrders.Sum(po => po.Price);
+                orderView.OrderAcceptDate = System.DateTime.UtcNow;
+            }
+
+            return orderViews;
         }
 
         public OrderFullView GetOrder(string userId, int orderId)
         {
-            throw new System.NotImplementedException();
+            var order = _db.Orders
+                .Where(e => e.CustomerId == userId)
+                .Single(e => e.Id == orderId);
+
+            var statuses = _db.HistoryOfOrderStatus
+                .Where(e => e.OrderId == order.Id)
+                .OrderBy(e => e.Date)
+                .Select(e => new StatusChangeView()
+                {
+                    Date = e.Date,
+                    NewStatus = e.NewStatus,
+                })
+                .ToArray();
+
+            var orderFullView = new OrderFullView()
+            {
+                Id = order.Id,
+                Status = order.Status,
+                ProductOrders = _db.GetProductOrderViews(order.Id),
+                StatusHistory = statuses,
+            };
+
+            orderFullView.Price = orderFullView.ProductOrders.Sum(po => po.Price);
+
+            return orderFullView;
         }
     }
 }
