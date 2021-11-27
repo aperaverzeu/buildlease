36fee4fdd252b20e25e390f869df26fe87af9dda
Никита Григорьевич Гришаев <nikita.grishaev@mail.ru>
2021-11-27 14:31:20 +0300

    axios token support (#31)

--- Buildlease/Buildlease/ClientApp/src/API.tsx
+++ Buildlease/Buildlease/ClientApp/src/API.tsx
@@ -3,32 +3,38 @@ import GetProductsRequest from "./components/Catalogue/Request/GetProductsReques
 import CategoryFilterView from "./components/Views/CategoryFilterView";
 import ProductView from "./components/Views/ProductView";
 import CategoryFullView from "./components/Views/CategoryFullView";
+import authService from "./components/api-authorization/AuthorizeService";
 
 const MainLink = 'https://localhost:5001/api/';
 
+async function AxiosTokenConfig() {
+  const token = await authService.getAccessToken();
+  return { headers: {'Authorization': `Bearer ${token}`} } ;
+}
+
 const API = {
 
-  GetAllCategories: () => {
+  GetAllCategories: async () => {
     return axios
       .post<CategoryFullView[]>(MainLink + 'GetAllCategories')
       .then(res => res.data);
   },
   
-  GetProducts: (info: GetProductsRequest) => {
+  GetProducts: async (info: GetProductsRequest) => {
     return axios
       .post<ProductView[]>(MainLink + 'GetProducts', info)
       .then(res => res.data);
   },
 
-  GetCategoryFilters: (categoryId: number) => {
+  GetCategoryFilters: async (categoryId: number) => {
     return axios
       .post<CategoryFilterView[]>(MainLink + `GetCategoryFilters/${categoryId}`)
       .then(res => res.data);
   },
 
-  SetProductOrderCount: (productId: number, count: number) => {
+  SetProductOrderCount: async (productId: number, count: number) => {
     return axios
-      .post<void>(MainLink + `SetProductOrderCount/${productId}/${count}`)
+      .post<void>(MainLink + `SetProductOrderCount/${productId}/${count}`, {}, await AxiosTokenConfig())
       .then(res => res.data);
   },
   
