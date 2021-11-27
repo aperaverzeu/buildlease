6ff2be180d74c6d56182b33128c656025eebb269
krastsislau <krastsislau@gmail.com>
2021-11-28 01:47:27 +0300

    styles upd

--- Buildlease/Buildlease/ClientApp/src/components/layout/MainContent.tsx
+++ Buildlease/Buildlease/ClientApp/src/components/layout/MainContent.tsx
@@ -1,6 +1,6 @@
 export default function MainContent(props: any) {
     return(
-        <div className='h-100 d-flex flex-grow-1'>
+        <div className='h-100 d-flex flex-column flex-grow-1'>
             {props.children}
         </div>
     );
