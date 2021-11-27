8c0c3b5fef8642a11bf3aa34862a8ea995cdf4a2
gorobot <gorobot.nz@gmail.com>
2021-11-27 15:39:07 +0300

    Orders created

--- Buildlease/Domain/EntitiesExample/OrderEntities.cs
+++ Buildlease/Domain/EntitiesExample/OrderEntities.cs
@@ -1,7 +1,27 @@
-﻿namespace Domain.EntitiesExample
+﻿using System.Collections.Generic;
+using Domain.Models;
+
+namespace Domain.EntitiesExample
 {
-    public class OrderEntities
+    public static class OrderEntities
     {
-        
+        public static List<Order> Get()
+        {
+            return new List<Order>()
+            {
+                new Order()
+                {
+                    Id = 1,
+                    CustomerId = "c65e9a5d-ef9f-450b-b851-f97190da3470",
+                    Status = OrderStatus.Cart
+                },
+                new Order()
+                {
+                    Id = 2,
+                    CustomerId = "f92039fd-5e51-431e-aefe-4a18e6cc846a",
+                    Status = OrderStatus.Finished
+                }
+            };
+        }
     }
 }
