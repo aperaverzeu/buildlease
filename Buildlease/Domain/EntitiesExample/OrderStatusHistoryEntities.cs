dda8c04929196289a58ca52b91042a1b8a0efe96
gorobot <gorobot.nz@gmail.com>
2021-11-27 16:03:13 +0300

    History created

--- Buildlease/Domain/EntitiesExample/OrderStatusHistoryEntities.cs
+++ Buildlease/Domain/EntitiesExample/OrderStatusHistoryEntities.cs
@@ -1,7 +1,52 @@
-﻿namespace Domain.EntitiesExample
+﻿using System;
+using System.Collections.Generic;
+using Domain.Models;
+using Domain.Models.Historicity;
+
+namespace Domain.EntitiesExample
 {
-    public class OrderStatusHistoryEntities
+    public static class OrderStatusHistoryEntities
     {
-        
+        public static List<HistoryOfOrderStatus> Get()
+        {
+            return new List<HistoryOfOrderStatus>()
+            {
+                new HistoryOfOrderStatus()
+                {
+                    Id = 1,
+                    OrderId = 4,
+                    Date = new DateTime(2020, 9, 20),
+                    NewStatus = OrderStatus.WaitingForApproval
+                },
+                new HistoryOfOrderStatus()
+                {
+                    Id = 2,
+                    OrderId = 4,
+                    Date = new DateTime(2020, 9, 25),
+                    NewStatus = OrderStatus.Approved
+                },
+                new HistoryOfOrderStatus()
+                {
+                    Id = 3,
+                    OrderId = 4,
+                    Date = new DateTime(2020, 10, 11),
+                    NewStatus = OrderStatus.DocumentPending
+                },
+                new HistoryOfOrderStatus()
+                {
+                    Id = 4,
+                    OrderId = 4,
+                    Date = new DateTime(2020, 10, 12),
+                    NewStatus = OrderStatus.InProcess
+                },
+                new HistoryOfOrderStatus()
+                {
+                    Id = 1,
+                    OrderId = 4,
+                    Date = new DateTime(2020, 10, 25),
+                    NewStatus = OrderStatus.Finished
+                }
+            };
+        }
     }
 }
