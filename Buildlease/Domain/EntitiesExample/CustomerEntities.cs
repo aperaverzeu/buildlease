5b2aa12c69343a63d56616737e6b6dac57d36cdd
gorobot <gorobot.nz@gmail.com>
2021-11-28 15:02:59 +0300

    Customers created

--- Buildlease/Domain/EntitiesExample/CustomerEntities.cs
+++ Buildlease/Domain/EntitiesExample/CustomerEntities.cs
@@ -1,7 +1,32 @@
-﻿namespace Domain.EntitiesExample
+﻿using System.Collections.Generic;
+using Domain.Models;
+
+namespace Domain.EntitiesExample
 {
     public class CustomerEntities
     {
-        
+        public static List<Customer> Get()
+        {
+            return new List<Customer>()
+            {
+                new Customer()
+                {
+                    UserId = "c65e9a5d-ef9f-450b-b851-f97190da3470", //User
+                    CompanyName = "Never Zero INC.",
+                    RepresentativeName = "NZINC",
+                    ContactInfo = "Email: contact@nzinc.com" +
+                                  "Phone: 88005553535"
+                },
+                new Customer()
+                {
+                    UserId = "f92039fd-5e51-431e-aefe-4a18e6cc846a", //ADMIN
+                    CompanyName = "Taiwan is not a legitimate state CORP",
+                    RepresentativeName = "Jade Chieftain Xi CORP",
+                    ContactInfo = "Email: contact@xi.com" +
+                                  "Phone: +7-321-99-84400928",
+                    JuridicalAddressId = null
+                },
+            };
+        }
     }
 }
