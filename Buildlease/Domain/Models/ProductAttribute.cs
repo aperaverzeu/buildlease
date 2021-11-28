50115565dd84ec863912b83debc41eb1530ef323
Сhesslav <vsvoiteshonok1@gmail.com>
2021-11-28 16:39:09 +0300

    Models configuring (#41)
    * some models configuration
    
    * relations modified
    
    * string constraints added
    
    * minor modifications
    
    * package-lock returned
    
    * refactoring
    
    * implementing remained attributes
    
    Co-authored-by: Rigorich <nikita.grishaev@mail.ru>

--- Buildlease/Domain/Models/ProductAttribute.cs
+++ Buildlease/Domain/Models/ProductAttribute.cs
@@ -7,5 +7,8 @@
         public int AttributeId { get; set; }
         public decimal? ValueNumber { get; set; }
         public string ValueString { get; set; }
+
+        public Attribute Attribute { get; set; }
+        public Product Product { get; set; }
     }
 }
---
+++ Buildlease/Persistence/Configurations/ProductAttribute.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Models;
+
+namespace Persistence.Configurations
+{
+    class ProductAttributeBuilder : IEntityTypeConfiguration<ProductAttribute>
+    {
+        public void Configure(EntityTypeBuilder<ProductAttribute> builder)
+        {
+            builder .ToTable("ProductAttribute")
+                    .HasKey(e => e.Id);
+
+            builder .Property(e => e.Id)
+                    .ValueGeneratedOnAdd()
+                    .IsRequired();
+
+            builder .Property(e => e.ProductId)
+                    .IsRequired();
+
+            builder .Property(e => e.AttributeId)
+                    .IsRequired();
+
+            builder .Property(e => e.ValueNumber)
+                    .IsRequired(false)
+                    .HasDefaultValue(null)
+                    .HasPrecision(10, 5);
+
+            builder .Property(e => e.ValueString)
+                    .IsRequired(false)
+                    .HasDefaultValue(null)
+                    .HasMaxLength(100);
+
+            builder .HasOne<Attribute>(pa => pa.Attribute)
+                    .WithMany(a => a.ProductAttributes)
+                    .HasForeignKey(pa => pa.AttributeId)
+                    .IsRequired()
+                    .OnDelete(DeleteBehavior.Cascade);
+
+            builder .HasOne<Product>(pa => pa.Product)
+                    .WithMany(p => p.ProductAttributes)
+                    .HasForeignKey(pa => pa.ProductId)
+                    .IsRequired()
+                    .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
