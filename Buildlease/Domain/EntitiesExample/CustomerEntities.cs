daec32036cb05a06a36842ed85407e1be1e5de91
gorobot <gorobot.nz@gmail.com>
2021-11-28 15:31:23 +0300

    Upd

--- Buildlease/Domain/EntitiesExample/CustomerEntities.cs
+++ Buildlease/Domain/EntitiesExample/CustomerEntities.cs
@@ -14,7 +14,7 @@ namespace Domain.EntitiesExample
                     UserId = "c65e9a5d-ef9f-450b-b851-f97190da3470", //User
                     CompanyName = "Never Zero INC.",
                     RepresentativeName = "NZINC",
-                    ContactInfo = "Email: contact@nzinc.com" +
+                    ContactInfo = "Email: contact@nzinc.com\n" +
                                   "Phone: 88005553535",
                     JuridicalAddressId = 4
                 },
@@ -23,7 +23,7 @@ namespace Domain.EntitiesExample
                     UserId = "f92039fd-5e51-431e-aefe-4a18e6cc846a", //ADMIN
                     CompanyName = "Taiwan is not a legitimate state CORP",
                     RepresentativeName = "Jade Chieftain Xi CORP",
-                    ContactInfo = "Email: contact@xi.com" +
+                    ContactInfo = "Email: contact@xi.com\n" +
                                   "Phone: +7-321-99-84400928",
                     JuridicalAddressId = null
                 },
