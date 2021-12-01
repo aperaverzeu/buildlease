ea25a68f7c24d93d71dd92823d800b395afe13e3
Rigorich <nikita.grishaev@mail.ru>
2021-12-01 21:44:06 +0300

    FUCK POSTGRESQL

--- Buildlease/Domain/EntitiesExample/OrderEntities.cs
+++ Buildlease/Domain/EntitiesExample/OrderEntities.cs
@@ -11,43 +11,43 @@ namespace Domain.EntitiesExample
             {
                 new Order()
                 {
-                    Id = 1,
+                    Id = -1,
                     CustomerId = "c65e9a5d-ef9f-450b-b851-f97190da3470",
                     Status = OrderStatus.WaitingForApproval //+
                 },
                 new Order()
                 {
-                    Id = 2,
+                    Id = -2,
                     CustomerId = "c65e9a5d-ef9f-450b-b851-f97190da3470",
                     Status = OrderStatus.InProcess //+
                 },
                 new Order()
                 {
-                    Id = 3,
+                    Id = -3,
                     CustomerId = "c65e9a5d-ef9f-450b-b851-f97190da3470",
                     Status = OrderStatus.InProcess //+
                 },
                 new Order()
                 {
-                    Id = 4,
+                    Id = -4,
                     CustomerId = "c65e9a5d-ef9f-450b-b851-f97190da3470",
                     Status = OrderStatus.Finished //+
                 },
                 new Order()
                 {
-                    Id = 5,
+                    Id = -5,
                     CustomerId = "c65e9a5d-ef9f-450b-b851-f97190da3470",
                     Status = OrderStatus.DeclinedByCustomer //+
                 },
                 new Order()
                 {
-                    Id = 6,
+                    Id = -6,
                     CustomerId = "f92039fd-5e51-431e-aefe-4a18e6cc846a",
                     Status = OrderStatus.Cart //+
                 },
                 new Order()
                 {
-                    Id = 7,
+                    Id = -7,
                     CustomerId = "f92039fd-5e51-431e-aefe-4a18e6cc846a",
                     Status = OrderStatus.InProcess //+
                 },
--- Buildlease/Domain/EntitiesExample/ProductAttributeEntities.cs
+++ Buildlease/Domain/EntitiesExample/ProductAttributeEntities.cs
@@ -11,94 +11,94 @@ namespace Domain.EntitiesExample
             {
                 new ProductAttribute()
                 {
-                    Id = 1,
-                    ProductId = 13,
-                    AttributeId = 10,
+                    Id = -1,
+                    ProductId = -13,
+                    AttributeId = -10,
                     ValueNumber = 100,
                 },
                 new ProductAttribute()
                 {
-                    Id = 2,
-                    ProductId = 13,
-                    AttributeId = 11,
-                    ValueNumber = 500,
+                    Id = -2,
+                    ProductId = -13,
+                    AttributeId = -11,
+                    ValueNumber = 500.42069m,
                 },
                 new ProductAttribute()
                 {
-                    Id = 3,
-                    ProductId = 9,
-                    AttributeId = 4,
+                    Id = -3,
+                    ProductId = -9,
+                    AttributeId = -4,
                     ValueNumber = 50
                 },
                 new ProductAttribute()
                 {
-                    Id = 4,
-                    ProductId = 4,
-                    AttributeId = 12,
+                    Id = -4,
+                    ProductId = -4,
+                    AttributeId = -12,
                     ValueNumber = 3
                 },
                 new ProductAttribute()
                 {
-                    Id = 5,
-                    ProductId = 4,
-                    AttributeId = 15,
+                    Id = -5,
+                    ProductId = -4,
+                    AttributeId = -15,
                     ValueString = "Steel"
                 },
                 new ProductAttribute()
                 {
-                    Id = 6,
-                    ProductId = 5,
-                    AttributeId = 15,
+                    Id = -6,
+                    ProductId = -5,
+                    AttributeId = -15,
                     ValueString = "Carbonised steel"
                 },
                 new ProductAttribute()
                 {
-                    Id = 7,
-                    ProductId = 7,
-                    AttributeId = 15,
+                    Id = -7,
+                    ProductId = -7,
+                    AttributeId = -15,
                     ValueString = "Titan"
                 },
                 new ProductAttribute()
                 {
-                    Id = 8,
-                    ProductId = 2,
-                    AttributeId = 1,
+                    Id = -8,
+                    ProductId = -2,
+                    AttributeId = -1,
                     ValueString = "Linden"
                 },
                 new ProductAttribute()
                 {
-                    Id = 9,
-                    ProductId = 1,
-                    AttributeId = 1,
+                    Id = -9,
+                    ProductId = -1,
+                    AttributeId = -1,
                     ValueString = "Pine"
                 },
 
                 new ProductAttribute()
                 {
-                    Id = 10,
-                    ProductId = 12,
-                    AttributeId = 9,
+                    Id = -10,
+                    ProductId = -12,
+                    AttributeId = -9,
                     ValueString = "Wood"
                 },
                 new ProductAttribute()
                 {
-                    Id = 11,
-                    ProductId = 13,
-                    AttributeId = 9,
+                    Id = -11,
+                    ProductId = -13,
+                    AttributeId = -9,
                     ValueString = "Plastic"
                 },
                 new ProductAttribute()
                 {
-                    Id = 12,
-                    ProductId = 12,
-                    AttributeId = 14,
+                    Id = -12,
+                    ProductId = -12,
+                    AttributeId = -14,
                     ValueNumber = 42
                 },
                 new ProductAttribute()
                 {
-                    Id = 13,
-                    ProductId = 13,
-                    AttributeId = 14,
+                    Id = -13,
+                    ProductId = -13,
+                    AttributeId = -14,
                     ValueNumber = 42
                 },
             };
