741c5c3dffab84733f7a843a39766ce81264e7cf
Rigorich <nikita.grishaev@mail.ru>
2021-12-02 16:38:11 +0300

    Admin Panel DTOs

--- Buildlease/Contracts/DTOs/CustomerInfo.cs
+++ Buildlease/Contracts/DTOs/CustomerInfo.cs
@@ -4,13 +4,12 @@ namespace Contracts.DTOs
 {
     public class CustomerInfo
     {
+        [JsonIgnore]
+        public string UserId { get; set; }
         public string CompanyName { get; set; }
         public string RepresentativeName { get; set; }
         public string ContactInfo { get; set; }
         public AddressInfo JuridicalAddress { get; set; }
         public AddressInfo[] DeliveryAddresses { get; set; }
-
-        [JsonIgnore]
-        public string UserId { get; set; }
     }
 }
---
+++ Buildlease/Contracts/DTOs/ProductInfo.ProductAttributeInfo.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Models;
+
+namespace Contracts.DTOs
+{
+    public class ProductAttributeInfo
+    {
+        public int Id { get; set; }
+        public int AttributeId { get; set; }
+        public string Name { get; set; }
+        public AttributeType ValueType { get; set; }
+
+        public string UnitName { get; set; }
+        // or
+        public string[] ExistingStringValues { get; set; }
+
+        public string Value { get; set; }
+    }
+}
