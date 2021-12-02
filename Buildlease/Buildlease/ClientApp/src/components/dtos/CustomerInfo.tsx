df2f7286d07be29af4c7cad2be8693a40783b13b
Rastsislau Kayko <krastsislau@gmail.com>
2021-12-02 14:01:41 +0300

    sorry

---
+++ Buildlease/Buildlease/ClientApp/src/components/dtos/CustomerInfo.tsx
@@ -0,0 +1,9 @@
+﻿import AddressInfo from "./AddressInfo";
+
+export default interface CustomerInfo {
+    CompanyName: string,
+    RepresentativeName: string,
+    ContactInfo: string,
+    JuridicalAddress: AddressInfo,
+    DeliveryAddresses: AddressInfo[],
+}
