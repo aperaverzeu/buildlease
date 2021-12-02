659cff483ce3f11e22f5f2b35c3548156f4cb172
Rigorich <nikita.grishaev@mail.ru>
2021-12-02 16:38:11 +0300

    Admin Panel DTOs

---
+++ Buildlease/Contracts/DTOs/CategoryInfo.AttribiteInfo.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Models;
+
+namespace Contracts.DTOs
+{
+    public class AttributeInfo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public AttributeType ValueType { get; set; }
+        public string UnitName { get; set; }
+    }
+}
---
+++ Buildlease/Contracts/DTOs/CategoryInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace Contracts.DTOs
+{
+    public class CategoryInfo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageLink { get; set; }
+        public AttributeInfo[] Attributes { get; set; }
+    }
+}
