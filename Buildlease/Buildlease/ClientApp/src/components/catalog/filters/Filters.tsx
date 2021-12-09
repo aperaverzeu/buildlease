c246e8764d36e628355252c554a290d51e3e8cd6
Rastsislau Kayko <krastsislau@gmail.com>
2021-12-10 02:06:36 +0300

    catalog price partial fixup

--- Buildlease/Buildlease/ClientApp/src/components/catalog/filters/Filters.tsx
+++ Buildlease/Buildlease/ClientApp/src/components/catalog/filters/Filters.tsx
@@ -18,10 +18,10 @@ export default function Filters({filtersInfo, filtration, setFiltration}: Filter
       }}
     >
       {filtersInfo
-          .filter(filter => filter.Values || (filter.MinValue && filter.MaxValue))
+          .filter(filter => filter.Values || (typeof filter.MinValue == "number" && typeof filter.MaxValue == "number") || filter.Id == 0) // the last one for the price filter
           .map(filter => {
-        if (filter.Values && (filter.UnitName || filter.MinValue || filter.MaxValue)) throw 'Undefined multi type filter';
-        if (!filter.Values && !(filter.UnitName && filter.MinValue && filter.MaxValue)) throw 'Undefined none type filter';
+        if (filter.Values && (filter.UnitName || typeof filter.MinValue == 'number' || typeof filter.MaxValue == 'number')) throw 'Undefined multi type filter';
+        if (!filter.Values && !(filter.UnitName && typeof filter.MinValue == "number" && typeof filter.MaxValue == 'number') && filter.Id != 0) throw 'Undefined none type filter';
         return <Item 
           key={filter.Id}
           CategoryFilterView={filter} 
--- Buildlease/Buildlease/ClientApp/src/components/catalog/requests/GetProductsRequest.tsx
+++ Buildlease/Buildlease/ClientApp/src/components/catalog/requests/GetProductsRequest.tsx
@@ -3,6 +3,7 @@ import SortRule from "./SortRule"
 
 export default interface GetProductsRequest {
   CategoryId: number,
+  MaxPrice: number | null,
   Filters: AttributeFilter[] | null,
   OrderByRule: SortRule | null,
   SkipCount: number,
