be51d2ea04f78c87c6795812072323cff4711c47
Rigorich <nikita.grishaev@mail.ru>
2022-05-20 18:38:49 +0300

    ProductDescriptions management endpoints

---
+++ Buildlease/Buildlease/Endpoints/ProductDescriptions/ProductDescription.cs
@@ -0,0 +1,9 @@
+﻿namespace Buildlease.Endpoints.ProductDescriptions
+{
+    public class ProductDescription
+    {
+        public int ProductId { get; set; }
+        public string Language { get; set; }
+        public string Description { get; set; }
+    }
+}
